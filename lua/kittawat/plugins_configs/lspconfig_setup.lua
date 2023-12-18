ec443dddfa1a1e80f040e12a20d78d8cc8f8ba23
gittawat <kittawat49254@gmail.com>
2023-12-18 20:52:26 +0700

    a bit of revisiting

--- lua/kittawat/plugins_configs/lspconfig_setup.lua
+++ lua/kittawat/plugins_configs/lspconfig_setup.lua
@@ -26,8 +26,8 @@ lsp_z.on_attach(function(client, bufnr)
     buffer = bufnr,
     preserve_mappings = false
   })
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  --vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -84,7 +84,7 @@ cmp.setup({
 --})
 
 require('lspconfig').bashls.setup{}
-require('lspconfig').pylsp.setup{}
+require('lspconfig').pyright.setup{}
 require('lspconfig').openscad_lsp.setup{}
 require('lspconfig').zls.setup{}
 require('lspconfig').clangd.setup {}
--- lua/kittawat/set.lua
+++ lua/kittawat/set.lua
@@ -1,5 +1,7 @@
 --vim.opt.guicursor = "a:ver25"1
 
+vim.opt.clipboard="unnamed,unnamedplus"
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
