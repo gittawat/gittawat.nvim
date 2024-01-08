eb4e6a5ca19e867a7e6c0b8d593162aff8746a3f
gittawat <kittawat49254@gmail.com>
2024-01-09 00:20:22 +0700

    add trouble and fidget

---
+++ lua/kittawat/plugins/trouble.lua
@@ -0,0 +1,11 @@
+local trouble_spec = {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		vim.keymap.set('n','<leader>tt',"<cmd>TroubleToggle workspace_diagnostics<cr>")
+		vim.keymap.set('n', '[t', '<cmd>lua require("trouble").next({skip_groups = true, jump = true})<cr>')
+		vim.keymap.set('n', ']t', '<cmd>lua require("trouble").previous({skip_groups = true, jump = true})<cr>')
+	end
+}
+
+return trouble_spec
