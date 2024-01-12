97d8a3f76608ca005468066ab7bc24c2db5dd310
gittawat <kittawat49254@gmail.com>
2024-01-12 22:51:09 +0700

    open vim_fugitvie in newtab :)

--- lua/kittawat/plugins/vim_fugitive.lua
+++ lua/kittawat/plugins/vim_fugitive.lua
@@ -1,6 +1,10 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>git", vim.cmd.Git);
+		vim.keymap.set("n", "<leader>git", function()
+			vim.cmd.Git()
+			local key_sequence = vim.api.nvim_replace_termcodes("<C-W>T", true, true, true)
+			vim.api.nvim_feedkeys(key_sequence, "n", true)
+		end);
 	end
 }
