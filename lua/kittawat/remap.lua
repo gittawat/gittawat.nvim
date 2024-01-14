60d91d3c64fe0a09caa51fbb7924093d31f0491d
gittawat <kittawat49254@gmail.com>
2024-01-14 16:17:49 +0700

    swap j and k

--- lua/kittawat/remap.lua
+++ lua/kittawat/remap.lua
@@ -1,15 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>/", vim.cmd.nohlsearch)
-vim.keymap.set("n", "<leader>c/",":let @/=''<Enter>")
+vim.keymap.set("n", "<leader>c/", ":let @/=''<Enter>")
+vim.cmd("map <leader>e <CR>")
 
 
 -- Set h for up, j for down, k for left, and l for right
--- vim.api.nvim_set_keymap('n', 'j', '<Up>', { noremap = true })
--- vim.api.nvim_set_keymap('n', 'k', '<Down>', { noremap = true })
--- vim.api.nvim_set_keymap('n', 'h', '<Left>', { noremap = true })
--- vim.api.nvim_set_keymap('n', 'l', '<Right>', { noremap = true })
--- vim.api.nvim_set_keymap('v', 'j', '<Up>', { noremap = true })
--- vim.api.nvim_set_keymap('v', 'k', '<Down>', { noremap = true })
--- vim.api.nvim_set_keymap('v', 'h', '<Left>', { noremap = true })
--- vim.api.nvim_set_keymap('v', 'l', '<Right>', { noremap = true })
+vim.keymap.set({ "n", "v" }, 'k', 'j', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, 'j', 'k', { noremap = true, silent = true })
 
+-- <C-space> toggle between normal and insert mode
+-- vim.keymap.set("n", '<C-space>', 'i', { noremap = true, silent = true })
+-- vim.keymap.set("i", '<C-space>', '<ESC>', { noremap = true, silent = true })
