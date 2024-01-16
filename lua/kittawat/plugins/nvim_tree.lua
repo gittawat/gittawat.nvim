d11ca05c75b8d3a8a8f1a3bcdd62cf86c36c3eb6
gittawat <kittawat49254@gmail.com>
2024-01-16 15:12:33 +0700

    <F5> as nvim-tree refresh

--- lua/kittawat/plugins/nvim_tree.lua
+++ lua/kittawat/plugins/nvim_tree.lua
@@ -7,5 +7,6 @@ return {
 	config = function()
 		require("nvim-tree").setup {}
 		vim.keymap.set("n", "<leader>ft" , "<cmd>lua vim.cmd.NvimTreeToggle()<cr>")
+		vim.keymap.set("n", "<F5>" , "<cmd>lua vim.cmd.NvimTreeRefresh()<cr>")
 	end
 }
