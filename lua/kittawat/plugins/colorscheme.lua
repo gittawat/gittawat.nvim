b30230e80a96ffa8f6e4bab86c1a0ab0c8ccbddf
gttwt <kittawat49254@gmail.com>
2025-11-25 21:04:02 +0700

    remove colorscheme + telescope 0.1.9

--- lua/kittawat/plugins/colorscheme.lua
+++ lua/kittawat/plugins/colorscheme.lua
@@ -1,24 +1,24 @@
-return {
-	{
-		'echasnovski/mini.base16',
-		version = false,
-		config = function()
-			if vim.fn.has("termguicolors") then
-				vim.opt.termguicolors = true
-			end
-			vim.cmd("set background=light")
-			vim.cmd("set background=light")
-			vim.cmd("colorscheme minicyan")
-			vim.cmd("colorscheme minicyan")
-		end
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			--			vim.cmd("colorscheme kanagawa")
-		end
-	},
-	{
-		"EdenEast/nightfox.nvim",
-	}
-}
+--return {
+--	{
+--		'echasnovski/mini.base16',
+--		version = false,
+--		config = function()
+--			if vim.fn.has("termguicolors") then
+--				vim.opt.termguicolors = true
+--			end
+--			vim.cmd("set background=light")
+--			vim.cmd("set background=light")
+--			vim.cmd("colorscheme minicyan")
+--			vim.cmd("colorscheme minicyan")
+--		end
+--	},
+--	{
+--		"rebelot/kanagawa.nvim",
+--		config = function()
+--			--			vim.cmd("colorscheme kanagawa")
+--		end
+--	},
+--	{
+--		"EdenEast/nightfox.nvim",
+--	}
+--}
